f102f16afd24e58fb4395ce973b42daf6910ba71
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-04-03 17:07:50 -0700

    set up webpack & .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # restaurant
+
+Classes
+Object creation constructors
+Getters and setters
+Computed names and class fields
+Private class fields and methods
+Inheritance
+Composition > Inheritance
+ES6 Modules pattern
+npm
+Webpack
+Webpack plugins and loaders
+Source mappi9ng
+
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+//   optimization: {
+//     runtimeChunk: 'single',
+//   },
+};
