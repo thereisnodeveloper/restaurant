b8e3786e5900ff664f7c2d7f8bdefaa054cbcac5
lamachin-dev <56517440+lamachin-dev@users.noreply.github.com>
2024-04-03 17:45:36 -0700

    finish home page skeleton

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ const img = new Image()
 img.src = Placeholder
 img.height = 100
 content.appendChild(img)
+
+const title = document.createElement("h1")
+title.textContent = "Bad Burgers"
+
+const description = document.createElement("p")
+description.textContent = `Welcome to our burger restaurant, where we serve up delicious, hearty burgers made with the freshest ingredients. Our menu features a wide variety of burger options, from classic cheeseburgers to specialty burgers with unique toppings. We pride ourselves on using only the highest quality beef, freshly baked buns, and locally sourced produce to create the perfect burger experience for our customers. In addition to burgers, we also offer a selection of sides, salads, and drinks to complete your meal. Whether you're grabbing a quick lunch or enjoying a night out with friends and family, we're confident that you'll love our food and our friendly, welcoming atmosphere. Come visit us today and see what sets us apart!`
+
+//append in groups
+content.append(title,description)
